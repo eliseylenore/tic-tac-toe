44244684d77cadcf1ef4ff66334502901f9e5fa2
Elise St Hilaire <eliseylenore@gmail.com>
2017-09-11 16:17:46 -0700

    Create controllers for 3 pages plus xunit tests

---
+++ TicTacToeTests/Controllers/GamePlayControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+using TicTacToe.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace TicTacToe.Controllers.Tests
+{
+    public class GamePlayControllerTests
+    {
+        [Fact()]
+        public void IndexTest()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+    }
+}
---
+++ TicTacToeTests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+using System.Web;
+using TicTacToe.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace TicTacToe.Controllers.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void IndexTest()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+
+        }
+    }
+}
